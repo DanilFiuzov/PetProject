1b7b63e24f6f348b6eb5a50ef48ff8ffe362e48d
PUP04ka2014 <PUP04kaDanil@gamil.com>
2025-12-11 13:21:44 +0800

    Beta 9.0.1

--- database.js
+++ database.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
 
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'GameCenter'
+// });
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'GameCenter'
 });
-
-// const connection = mysql.createConnection({
-//     host: '127.0.0.1',
-//     user: 'root',
-//     password: '',
-//     database: 'GameCenter'
-// });
 
 //Аккаунт
 connection.connect((err) => {
